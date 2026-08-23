12d2601a102ad4af6ce30ccffa43cc69ea3267b9
Mansi Yadav <mansiyadav2807@gmail.com>
2026-08-23 14:12:38 +0530

    Time: 6 ms (97.08%), Space: 47.3 MB (10.95%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean sumGame(String num) {
         int n = num.length();
-
         int leftSum = 0, rightSum = 0;
         int leftQ = 0, rightQ = 0;
-
         for (int i = 0; i < n / 2; i++) {
             char c = num.charAt(i);
-
             if (c == '?') {
                 leftQ++;
             } else {
                 leftSum += c - '0';
             }
         }
-
         for (int i = n / 2; i < n; i++) {
             char c = num.charAt(i);
-
             if (c == '?') {
                 rightQ++;
             } else {
