298e668e5c7b07e7aa7f10978c1a198e752a43ef
Mansi Yadav <mansiyadav2807@gmail.com>
2026-09-14 12:06:03 +0530

    Time: 0 ms (100%), Space: 42.2 MB (96.35%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {       
         return rec1[0] < rec2[2] &&
                rec1[2] > rec2[0] &&
                rec1[1] < rec2[3] &&
