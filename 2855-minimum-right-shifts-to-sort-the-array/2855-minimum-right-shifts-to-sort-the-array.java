ceb407620fa8a791bbdd2a7cce531de9dfc48049
Mansi Yadav <mansiyadav2807@gmail.com>
2026-09-19 11:29:52 +0530

    Time: 1 ms (100%), Space: 44.6 MB (40.82%) - LeetHub

--- 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.java
+++ 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.java
@@ -1,31 +1,24 @@
 class Solution {
     public int minimumRightShifts(List<Integer> nums) {
-        
         int n = nums.size();
         int count = 0;
         int index = -1;
-
         for (int i = 0; i < n - 1; i++) {
             if (nums.get(i) > nums.get(i + 1)) {
                 count++;
                 index = i;
             }
         }
-
-        // Check circular pair: last element -> first element
         if (nums.get(n - 1) > nums.get(0)) {
             count++;
             index = n - 1;
         }
-
         if (count > 1) {
             return -1;
         }
-
         if (count == 0) {
             return 0;
         }
-
         return n - 1 - index;
     }
 }
